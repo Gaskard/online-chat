6f97497fdb0f0ca69dd0a632b2ee245eb77e271f
Dmytro Shaposhnikov <dimacr137@gmail.com>
2025-08-08 13:13:45 +0300

    add ChatsList item

--- src/components/chatsList/ChatsList.tsx
+++ src/components/chatsList/ChatsList.tsx
@@ -1,4 +1,7 @@
 import './chatsList.scss'
+import photo1 from '../contactList/img/photo1.svg'
+import photo2 from '../contactList/img/photo2.svg'
+import photo3 from '../contactList/img/photo3.svg'
 
 import '../searchInput/SearchInput.tsx'
 import SearchInput from "../searchInput/SearchInput.tsx";
@@ -9,33 +12,42 @@ const ChatsList = () => {
             <SearchInput/>
             <ul className="chats">
                 <li className="chats__item">
-                    <div className="chats__item-info">
-                        <div className="chats__item-name">Athalia Putri</div>
-                        <div className="chats__item-message">Good morning, did you sleep well?</div>
-                    </div>
-                    <div className="chats__item-meta">
-                        <div className="chats__item-date">Today</div>
-                        <div className="chats__item-count">1</div>
-                    </div>
-                </li>
-                <li className="chats__item">
-                    <div className="chats__item-info">
-                        <div className="chats__item-name">Raki Devon</div>
-                        <div className="chats__item-message">How is it going?</div>
-                    </div>
-                    <div className="chats__item-meta">
-                        <div className="chats__item-date">17/6</div>
-                        <div className="chats__item-count"></div>
+                    <img src={photo1} alt="" className="chats__item-img"/>
+                    <div className="chats__item-data">
+                        <div className="chats__item-info">
+                            <div className="chats__item-name">Athalia Putri</div>
+                            <div className="chats__item-message">Good morning, did you sleep well?</div>
+                        </div>
+                        <div className="chats__item-meta">
+                            <div className="chats__item-date">Today</div>
+                            <div className="chats__item-count">1</div>
+                        </div>
                     </div>
                 </li>
                 <li className="chats__item">
-                    <div className="chats__item-info">
-                        <div className="chats__item-name"></div>
-                        <div className="chats__item-message"></div>
+                    <img src={photo2} alt="" className="chats__item-img"/>
+                    <div className="chats__item-data">
+                        <div className="chats__item-info">
+                            <div className="chats__item-name">Raki Devon</div>
+                            <div className="chats__item-message">How is it going?</div>
+                        </div>
+                        <div className="chats__item-meta">
+                            <div className="chats__item-date">17/6</div>
+                            <div className="chats__item-count"></div>
+                        </div>
                     </div>
-                    <div className="chats__item-meta">
-                        <div className="chats__item-date"></div>
-                        <div className="chats__item-count"></div>
+                </li>
+                <li className="chats__item">
+                    <img src={photo3} alt="" className="chats__item-img"/>
+                    <div className="chats__item-data">
+                        <div className="chats__item-info">
+                            <div className="chats__item-name">Erlan Sadewa</div>
+                            <div className="chats__item-message">Aight, noted</div>
+                        </div>
+                        <div className="chats__item-meta">
+                            <div className="chats__item-date">17/6</div>
+                            <div className="chats__item-count">1</div>
+                        </div>
                     </div>
                 </li>
             </ul>
