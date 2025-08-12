4df0996011e2ef34aa923bae35e10b2d3447ecae
Dmytro Shaposhnikov <dimacr137@gmail.com>
2025-08-12 06:13:43 +0300

    add openChatBottomMenu

--- src/components/openChatBottomMenu/OpenChatBottomMenu.tsx
+++ src/components/openChatBottomMenu/OpenChatBottomMenu.tsx
@@ -1,12 +1,19 @@
 import './openChatBottomMenu.scss'
 
+import plus from './icons/plus.svg'
+import send from './icons/send.svg'
+
 const OpenChatBottomMenu = () => {
     return (
         <div className="container">
             <div className="chat-form">
-                <img src="" alt="" className="chat-form__attach"/>
+                <button className="chat-form__attach-button">
+                    <img src={plus} alt="attach file" className="chat-form__plus-icon"/>
+                </button>
                 <input type="text" className="chat-form__input"/>
-
+                <button className="chat-form__send-button">
+                    <img src={send} alt="send message" className="chat-form__send-icon"/>
+                </button>
             </div>
         </div>
     )
