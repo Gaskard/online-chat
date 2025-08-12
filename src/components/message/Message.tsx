7fcce3c3e4f808055c3a7122d8be78605f43542f
Dmytro Shaposhnikov <dimacr137@gmail.com>
2025-08-12 21:28:15 +0300

    add message

---
+++ src/components/message/Message.tsx
@@ -0,0 +1,15 @@
+import './message.scss'
+
+const Message = () => {
+    return (
+        <div className="message">
+            <div className="message__text">K, I'm on my way</div>
+            <div className="message__info">
+                <div className="message__info-date">09.13</div>
+                <div className="message__info-status">Read</div>
+            </div>
+        </div>
+    )
+} // Добавить стили для чужого сообщения
+
+export default Message
--- src/components/openChatMessages/OpenChatMessages.tsx
+++ src/components/openChatMessages/OpenChatMessages.tsx
@@ -1,10 +1,12 @@
 import './openChatMessages.scss'
 
+import Message from "../message/Message.tsx";
+
 const OpenChatMessages = () => {
     return (
         <div className="container">
             <ul className="chat-area">
-
+                <Message/>
             </ul>
         </div>
     )
