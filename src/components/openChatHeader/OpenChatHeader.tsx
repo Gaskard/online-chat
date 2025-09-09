65316707f0c8a7ba76578f9aa2287cee557871fb
Dmytro Shaposhnikov <dimacr137@gmail.com>
2025-09-09 20:42:21 +0300

    welcome complete

--- src/components/openChatHeader/OpenChatHeader.tsx
+++ src/components/openChatHeader/OpenChatHeader.tsx
@@ -3,14 +3,22 @@ import './openChatHeader.scss'
 import back from './icons/back.svg'
 import burger from './icons/burger.svg'
 import search from './icons/search.svg'
+import {useNavigate} from "react-router-dom";
 
 const OpenChatHeader = () => {
+
+    const navigate = useNavigate()
+
     return (
         <div className="container">
             <div className="chat">
                 <header className="chat__header">
                     <div className="chat__header-left">
-                        <button className="chat__header-left-back"><img src={back} alt="back"/></button>
+                        <button
+                            onClick={() => navigate(-1)}
+                            className="chat__header-left-back">
+                            <img src={back} alt="back"/>
+                        </button>
                         <div className="chat__header-left-name">Athalia Putri</div>
                     </div>
                     <div className="chat__header-right">
--- src/components/welcomeScreen/WelcomeScreen.tsx
+++ src/components/welcomeScreen/WelcomeScreen.tsx
@@ -6,7 +6,7 @@ const WelcomeScreen = () => {
         <div className="container">
             <div className="welcome">
                 <img src={startMenu} alt="start menu" className="welcome__img"/>
-                <h2 className="welcome__title">Connect easily with your family and friends over countries</h2>
+                <h2 className="welcome__title">Connect easily with <br/> your family and friends <br/> over countries</h2>
                 <a href="" className="welcome__terms">Terms & Privacy Policy</a>
                 <button className="welcome__start">Start Messaging</button>
             </div>
